fe1c8e90ac93b3536717477603d9999fc3be0ca7
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2021-04-01 11:27:40 +0300

    Initial Commit

---
+++ Shared/Image_Gallery_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Gallery_DemoApp.swift
+//  Shared
+//
+//  Created by Tariq Almazyad on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Image_Gallery_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
